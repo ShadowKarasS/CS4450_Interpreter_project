0a920f1907e41cfdc1f0c87a5fb92b637113cf63
Ziang Xu <ziangxu@icloud.com>
2020-12-16 20:29:16 -0600

    change

--- interpreter.py
+++ interpreter.py
@@ -3,16 +3,16 @@ from lexer import Lexer,Token,Tokens
 #import parser
 
 def interpreter():
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 2: # check for input file exist
         print("Need more information!")
         return
-    file = open(sys.argv[1]).read
+    file = open(sys.argv[1]).read #read input file
     tokens = Lexer(file)
     token = lexer.build()
-    while token.ttype != Tokens.EMP:
+    while token.ttype != Tokens.EMP: # create tokens
         token = lexer.build()
     print(tokens)
-    parser = Parser(tokens)
+    parser = Parser(tokens) #input tokens into parser
 
     
 interpreter()
--- lexer.py
+++ lexer.py
@@ -2,11 +2,11 @@ import enum
 import sys
 
 
-class Token:
+class Token: #set and save information for the tokens
     def __init__(self,info,ttype):
         self.info = info
         self.ttype = ttype
-class Tokens(enum.Enum):
+class Tokens(enum.Enum): #tokens enum class
     EMP='\n'      #\0
     PLUS='+'     #+
     MINUS='-'    #-
@@ -40,38 +40,38 @@ class Tokens(enum.Enum):
     NUMBER=None
 class Lexer:
     token = None
-    def __init__(self,file):
+    def __init__(self,file): #set the starting loaction of the reading
         self.input = file
         self.info = '\0'
         self.location = -1
         self.next()
-    def next(self):
+    def next(self):    #iteration of the next location
         self.location += 1
-        if self.location > len(self.input):
+        if self.location > len(self.input): # check the end of line
             self.info = '\0'
         else:
             self.info = self.input[self.location]
-    def nextInfo(self):
+    def nextInfo(self): # check the info stored in the next location
         if self.location + 1 > len(self.input):
             return'\0'
         else:
             return self.input[self.location + 1]
-    def build(self):
+    def build(self): # compare the information to get the token
         while self.info == ' ' or self.info == '\t' or self.info == '\r':
             self.next()
-        while self.into == '#':
+        while self.into == '#': #skip all comment
             while self.info != '\n':
                 self.next()
-        if self.info == '\0':
+        if self.info == '\0': #check the if it is the end
             token = Token(self.info,Tokens.EMP)
             print(self.info)
-        elif self.info == '\t':
+        elif self.info == '\t': # check to indentation
             start.self.location
             while self.nextInfo() == '\t':
                 self.next()
             token = Token(self.info[start: self.location + 1],Tokens.INDENT)
             print(self.info)
-        elif self.info == '+':
+        elif self.info == '+': #check for + or +=
             if self.nextInfo()== '=':
                 inf = self.info
                 self.next()
@@ -80,7 +80,7 @@ class Lexer:
             else:
                 token = Token(self.info, Tokens.PLUS)
                 print(self.info)
-        elif self.info == '-':
+        elif self.info == '-': #check for - or-=
             if self.nextInfo()== '=':
                 inf = self.info
                 self.next()
@@ -89,7 +89,7 @@ class Lexer:
             else:
                 token = Token(self.info, Tokens.MINUS)
                 print(self.info)
-        elif self.info == '*':
+        elif self.info == '*': #check for * or *=
             if self.nextInfo()== '=':
                 inf = self.info
                 self.next()
@@ -98,7 +98,7 @@ class Lexer:
             else:
                 token = Token(self.info, Tokens.TIMES)
                 print(self.info)
-        elif self.info == '/':
+        elif self.info == '/': #check for / or /=
             if self.nextInfo()== '=':
                 inf = self.info
                 self.next()
@@ -107,7 +107,7 @@ class Lexer:
             else:
                 token = Token(self.info, Tokens.DIVID)
                 print(self.info)
-        elif self.info == '%':
+        elif self.info == '%': # check for % or %=
             if self.nextInfo()== '=':
                 inf = self.info
                 self.next()
@@ -116,7 +116,7 @@ class Lexer:
             else:
                 token = Token(self.info, Tokens.REMAINDER)
                 print(self.info)
-        elif self.info == '^':
+        elif self.info == '^': #check for ^ or ^=
             if self.nextInfo()== '=':
                 inf = self.info
                 self.next()
@@ -125,7 +125,7 @@ class Lexer:
             else:
                 token = Token(self.info, Tokens.EXP)
                 print(self.info)
-        elif self.info == '<':
+        elif self.info == '<': #check for < or <=
             if self.nextInfo()== '=':
                 inf = self.info
                 self.next()
@@ -134,7 +134,7 @@ class Lexer:
             else:
                 token = Token(self.info, Tokens.GREAT)
                 print(self.info)
-        elif self.info == '>':
+        elif self.info == '>': #check for > or >=
             if self.nextInfo()== '=':
                 inf = self.info
                 self.next()
@@ -143,7 +143,7 @@ class Lexer:
             else:
                 token = Token(self.info, Tokens.LESS)
                 print(self.info)
-        elif self.info == '=':
+        elif self.info == '=': #check for = or ==
             if self.nextInfo()== '=':
                 inf = self.info
                 self.next()
@@ -152,7 +152,7 @@ class Lexer:
             else:
                 token = Token(self.info, Tokens.EQU)
                 print(self.info)
-        elif self.info == '!':
+        elif self.info == '!': #check for !=
             if self.nextInfo() == '=':
                 inf = self.info
                 self.next()
@@ -160,19 +160,19 @@ class Lexer:
                 print(self.info)
             else:
                 self.next()
-        elif self.info == ':':
+        elif self.info == ':': #check for :
             token = Token(self.info,Tokens.COL)
             print(self.info)
-        elif self.info == '(':
+        elif self.info == '(': #check for (
             token = Token(self.info,Tokens.LP)
             print(self.info)
-        elif self.info == ')':
+        elif self.info == ')': #check for )
             token = Token(self.info,Tokens.RP)
             print(self.info)
-        elif self.info == ',':
+        elif self.info == ',': #check for ,
             token = Token(self.info,Tokens.COMMA)
             print(self.info)
-        elif self.info.is_integer() == True:
+        elif self.info.is_integer() == True: #check for numbers
             start = self.location
             while self.nextInfo.in_integer() == True:
                 self.next()
@@ -182,22 +182,22 @@ class Lexer:
                     self.next()
             token = Token(self.info[start: self.location + 1],Tokens.NUMBER)
             print(self.info)
-        elif self.info.isalpha() == True:
+        elif self.info.isalpha() == True: #check for words
             start = self.location
             while self.nextInfo().isalnum() == True or self.nextInfo() == '_':
                 self.next()
             text = self.info[start: self.location + 1]
             print(self.info)
-            if text == 'while':
+            if text == 'while': #check if words is keyword while
                 token = Token(text, Tokens.WHILE)
                 print(self.info)
-            if text == 'if':
+            if text == 'if': #check if words is keyword if
                 token = Token(text, Tokens.IF)
                 print(self.info)
-            if text == 'else':
+            if text == 'else': #check if words is keyword else
                 token = Token(text, Tokens.ELSE)
                 print(self.info)
-            if text == 'ELIF':
+            if text == 'ELIF': #check if words is keyword elif
                 token = Token(text, Tokens.ELIF)
                 print(self.info)
         self.next()
